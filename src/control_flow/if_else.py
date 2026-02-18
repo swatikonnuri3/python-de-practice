dabfd88b3d86577de7284a2c96f8cc3788bff962
swati konnuri <swatikonnuri@gmail.com>
2026-02-18 12:39:08 +0530

    Added control flow examples using if-else

---
+++ src/control_flow/if_else.py
@@ -0,0 +1,17 @@
+age = 20
+
+if age >= 18:
+    print("You are eligible to vote")
+else:
+    print("You are not eligible to vote")
+
+
+# Example 2: if-elif-else
+marks = 75
+
+if marks >= 90:
+    print("Grade: A")
+elif marks >= 60:
+    print("Grade: B")
+else:
+    print("Grade: C")
