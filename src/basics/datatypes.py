008d48795c876b396193b9d2f841b8f0107975c2
swati konnuri <swatikonnuri@gmail.com>
2026-02-18 11:55:45 +0530

    Added basics: variables and data types

---
+++ src/basics/datatypes.py
@@ -0,0 +1,14 @@
+"""
+Topic: Data Types in Python
+Author: Swati Konnuri
+"""
+
+name = "Swati"
+age = 21
+cgpa = 9.35
+is_student = True
+
+print(name, type(name))
+print(age, type(age))
+print(cgpa, type(cgpa))
+print(is_student, type(is_student))
