3eb9bdc554ad675193df2691120d2c069642c54b
swati konnuri <swatikonnuri@gmail.com>
2026-02-18 13:40:04 +0530

    Update functions examples

--- src/functions/functions_intro.py
+++ src/functions/functions_intro.py
@@ -11,3 +11,15 @@ def greet(name):
 
 print("Sum:", add(10, 20))
 greet("Swati")
+
+
+def calculate_factorial(n):
+    """
+    Calculates factorial of a number
+    """
+    result = 1
+    for i in range(1, n + 1):
+        result = result * i
+    return result
+fact = calculate_factorial(5)
+print("Factorial:", fact)
