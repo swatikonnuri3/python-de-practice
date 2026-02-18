3e7e7fc793414553933ee576e95194dfec455d31
swati konnuri <swatikonnuri@gmail.com>
2026-02-18 12:00:40 +0530

    Added functions and lambda examples

---
+++ src/functions/functions_intro.py
@@ -0,0 +1,13 @@
+"""
+Topic: Functions in Python
+Author: Swati Konnuri
+"""
+
+def add(a, b):
+    return a + b
+
+def greet(name):
+    print("Hello", name)
+
+print("Sum:", add(10, 20))
+greet("Swati")
---
+++ src/functions/lambda_functions.py
@@ -0,0 +1,10 @@
+"""
+Topic: Lambda Functions
+Author: Swati Konnuri
+"""
+
+add = lambda a, b: a + b
+square = lambda x: x * x
+
+print("Add:", add(5, 3))
+print("Square:", square(4))
